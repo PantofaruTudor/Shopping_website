03fbf3b6bb7117967c13a066fb9f0300e9378e60
PantofaruTudor <tudorpantofaru@gmail.com>
2025-02-15 14:20:05 +0200

    initial commit

---
+++ website.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const listItems = document.querySelectorAll('.categorii li');
+    let hoverTimeout;
+
+    listItems.forEach(item => {
+        const className = item.classList[0];
+        const dropdown = document.querySelector(`.${className}_dropdown`);
+
+        const showDropdown = () => {
+            clearTimeout(hoverTimeout);
+            dropdown.classList.add('show');
+        };
+
+        const hideDropdown = () => {
+            hoverTimeout = setTimeout(() => {
+                dropdown.classList.remove('show');
+            }, 400); // Adjust delay as needed
+        };
+
+        if (dropdown) {
+            item.addEventListener('mouseenter', showDropdown);
+            item.addEventListener('mouseleave', () => {
+                hoverTimeout = setTimeout(hideDropdown, 400);
+            });
+
+            dropdown.addEventListener('mouseenter', () => {
+                clearTimeout(hoverTimeout);
+                dropdown.classList.add('show');
+            });
+
+            dropdown.addEventListener('mouseleave', hideDropdown);
+        }
+    });
+});
+
