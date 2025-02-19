1a6d4955fe8de7ccc6910e4cd0560cc35bcaa50c
PantofaruTudor <tudorpantofaru@gmail.com>
2025-02-19 15:12:59 +0200

    finished the main-menu, sub-menu and the dropdown_menu structure, now working on the items and main page

--- website.js
+++ website.js
@@ -12,15 +12,15 @@ document.addEventListener('DOMContentLoaded', () => {
         });
         dropdown_area.classList.remove('show');
     }
-    const listItems = document.querySelectorAll('.categorii li');
+    let dropdown_area_active = false
     let hoverTimeout;
+    const listItems = document.querySelectorAll('.categorii li');
     const dropdown_area = document.querySelector('.dropdown_area')
-    let activeItem;
 
     listItems.forEach(item => {
         const className = item.classList[0];
         const dropdown = document.querySelector(`.${className}_dropdown`);
-        console.log(dropdown)
+
         if(dropdown) {
             dropdown.classList.add('hide')
             item.addEventListener('mouseenter', ()=>{
@@ -29,39 +29,34 @@ document.addEventListener('DOMContentLoaded', () => {
                     dropdown_area.classList.add('show')
                     dropdown.classList.remove('hide')
                 },300)  
-                activeItem = dropdown
 
                 
             });
             item.addEventListener('mouseleave', () => {
-                if(hoverTimeout)
-                {
-                    clearTimeout(hoverTimeout)
-                }
-                
-                hoverTimeout = setTimeout(()=> {
-                    if(!dropdown_area.classList.contains('show'))
+                hoverTimeout = setTimeout(()=> {                
+                    if(!dropdown_area.classList.contains('show') || dropdown_area_active === false)
                     {
                         dropdown.classList.add('hide');
                         dropdown_area.classList.remove('show')
                     }
-                    
-                    
                 }, 300)
             });
  
             dropdown_area.addEventListener('mouseenter',()=>{
+                dropdown_area_active = true
                 if(!dropdown.classList.contains('hide'))
                 {
                     dropdown_area.classList.add('show')
                     dropdown.classList.remove('hide')
                 }
                 
-                console.log(dropdown)
             })            
 
             dropdown_area.addEventListener('mouseleave',()=>{
+                dropdown_area_active = false
                 dropdown_area.classList.remove('show')
+                dropdown.classList.add('hide')
+
             })
 
         }
