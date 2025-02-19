c6847180cfaac7e2f665c215372d3460ce043ca3
PantofaruTudor <tudorpantofaru@gmail.com>
2025-02-20 01:22:20 +0200

    working on banner slider

--- website.js
+++ website.js
@@ -3,7 +3,7 @@ main_menu_segments.forEach(item => {
     item.style.gap = "20px";
 })
 
-
+/* DROPDOWN-MENU //////////////////////////////////////////////////////*/
 document.addEventListener('DOMContentLoaded', () => {
     function hideAllDropdowns() {
         const allDropdowns = document.querySelectorAll('.dropdown_info ul');
@@ -63,3 +63,46 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+/* BANNER-IMAGE SLIDER ///////////////////////////////*/ 
+const slides = document.querySelectorAll(".slider-container img")
+let slideIndex = 0
+let intervalId = null
+document.addEventListener("DOMContentLoaded",initializeSlider)
+function initializeSlider()
+{
+    if(slides.length > 0)
+    {
+        console.log(slides)
+        slides[slideIndex].classList.add("displaySlide")
+        intervalId=setInterval(nextSlide, 5000)
+    }
+}
+function showSlide(index)
+{
+    if(index >= slides.length)
+    {
+        slideIndex = 0
+    }
+    else if(index < 0)
+    {
+        slideIndex = slides.length - 1
+    }
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide")
+    })
+    slides[slideIndex].classList.add("displaySlide")
+}
+
+function prevSlide()
+{
+    clearInterval(intervalId)
+    slideIndex--
+    showSlide(slideIndex)
+}
+
+function nextSlide()
+{
+    slideIndex++
+    showSlide(slideIndex)
+}
+    
